de1681ae0bb9aa9472c1abab345fe4ffcaa6fdb2
appie1312 <agoeswag@gmail.com>
2025-09-08 09:45:24 +0200

    standard template

---
+++ app/config/config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * De database verbindingsgegevens
+ */
+define('DB_HOST', 'sneakerness');
+define('DB_NAME', 'sneakerness');
+define('DB_USER', 'host');
+define('DB_PASS', '');
+
+
+/**
+ * De naam van de virtualhost
+ */
+define('URLROOT', 'sneakerness');
+
+/**
+ * Het pad naar de folder app
+ */
+define('APPROOT', dirname(dirname(__FILE__)));
