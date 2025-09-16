3938eb71a4d0ab87b81d0c6f59a466e86757b9ab
appie1312 <agoeswag@gmail.com>
2025-09-16 13:48:23 +0200

    navbar in header

--- app/views/homepages/index.php
+++ app/views/homepages/index.php
@@ -1,4 +1,3 @@
-<?php require_once APPROOT . '/views/includes/header.php'; ?>
 
 <!doctype html>
 <html lang="en">
@@ -17,28 +16,7 @@
 
 <body>
   <header>
-    <nav class="navbar navbar-expand-lg  bg-light bg-opacity-50">
-      <div class="container-fluid">
-        <a class="navbar-brand fw-bold" href="/<?php echo URLROOT; ?>/homepages/index">SNEAKERNESS</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/<?php echo URLROOT; ?>/homepages/index">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" aria-current="page" href="/<?php echo URLROOT; ?>#">nog leeg</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" aria-current="page" href="/<?php echo URLROOT; ?>#">nog leeg</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <?php require_once APPROOT . '/views/includes/header.php'; ?>
 
     <img src="/public/img/wallpaper.jpg" alt="" class="hero">
 
