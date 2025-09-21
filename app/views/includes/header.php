ba418a5ef28cd3ce13005a9ad8786393bd5623b3
appie1312 <agoeswag@gmail.com>
2025-09-21 23:35:25 +0200

    header gefixt

--- app/views/includes/header.php
+++ app/views/includes/header.php
@@ -1,17 +1,3 @@
-<<<<<<< Updated upstream
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>MVC OOP PDO</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="<?= URLROOT; ?>/public/css/style.css">
-    <link rel="shortcut icon" href="<?= URLROOT; ?>/public/img/favicon.ico" type="image/x-icon">
-  </head>
-<body>
-=======
     <nav class="navbar navbar-expand-lg  bg-light bg-opacity-50">
       <div class="container-fluid">
         <a class="navbar-brand fw-bold" href="/<?php echo URLROOT; ?>/homepages/index">SNEAKERNESS</a>
@@ -34,4 +20,3 @@
         </div>
       </div>
     </nav>
->>>>>>> Stashed changes
