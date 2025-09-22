3b61f8540e357d765a3c77c56c681176eb343482
OdiMatar <340712@student.mboutrecht.nl>
2025-09-22 09:16:05 +0200

    Model map aangepast

--- app/controllers/Event.php
+++ app/controllers/Event.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../model/EventModel.php';
+require_once __DIR__ . '/../models/EventModel.php';
 require_once __DIR__ . '/../libraries/Database.php';
 
 use App\Model\EventModel;
