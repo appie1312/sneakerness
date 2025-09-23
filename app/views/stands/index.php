9f924719f8283eff34dd05c5b91e042557f3d2b6
Simon <341037@student.mboutrecht.nl>
2025-09-23 10:26:58 +0200

    style-stands

--- app/views/stands/index.php
+++ app/views/stands/index.php
@@ -4,9 +4,33 @@
     <meta charset="UTF-8">
     <title>Stands</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/public/css/Stands.css">
 </head>
 <body>
 <?php require_once APPROOT . '/views/includes/header.php'; ?>
+
+
+
+<body>
+  <div class="wrap">
+    <div class="row-head">
+      <div class="row-title">Aanwezige Stands</div>
+    </div>
+
+    <div class="rail">
+      <div class="card">Stand A1</div>
+      <div class="card">Stand B4</div>
+      <div class="card">Stand C7</div>
+      <div class="card">Stand D2</div>
+      <div class="card">Stand E5</div>
+      <div class="card">Stand F3</div>
+    </div>
+  </div>
+</body>
+</html>
+
+
+  
 
 </body>
 </html>
