5188a977adcba18c542a8fd7284065adc18ddeee
OdiMatar <340712@student.mboutrecht.nl>
2025-10-13 10:00:15 +0200

    Melding verplatst naar boven

--- app/views/event/index.php
+++ app/views/event/index.php
@@ -16,6 +16,13 @@ if (!isset($successMessage)) {
 }
 ?>
 
+<?php if (!empty($successMessage)): ?>
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+          <?= htmlspecialchars($successMessage) ?>
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+      <?php endif; ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -107,12 +114,7 @@ if (!isset($successMessage)) {
   </div>
 </div>
 
-<?php if (!empty($successMessage)): ?>
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-          <?= htmlspecialchars($successMessage) ?>
-          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-      <?php endif; ?>
+
 
  
 <div class="container footer container-fluid"> <footer class="py-3 my-4"> <ul class="nav justify-content-center border-bottom pb-3 mb-3 "> <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li> <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li> <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li> <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li> <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li> </ul> <p class="text-center text-body-secondary">© 2025 Company, Inc</p> </footer> </div>
